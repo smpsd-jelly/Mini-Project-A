d7b936061ff8edace278cc52a86b331ca1634677
panipak <panipaksenmee@gmail.com>
2026-05-25 14:02:14 +0700

    enhance: [MPA-16] move file to infrastructure

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
 	"os"
 	"time"
+
+	"mini-project-a/internal/infrastructure/database"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -17,7 +18,7 @@ func main() {
 		fmt.Println("Error loading .env file, using environment variables")
 	}
 
-	db, err := connectPostgres()
+	db, err := database.ConnectPostgres()
 	if err != nil {
 		panic(err)
 	}
@@ -37,41 +38,4 @@ func main() {
 	}
 
 	router.Run(":" + port)
-}
-
-func connectPostgres() (*sql.DB, error) {
-	host := os.Getenv("DB_HOST")
-	if host == "" {
-		host = "localhost"
-	}
-	port := os.Getenv("DB_PORT")
-	if port == "" {
-		port = "5432"
-	}
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbName := os.Getenv("DB_NAME")
-	sslMode := os.Getenv("DB_SSLMODE")
-
-	dsn := fmt.Sprintf(
-		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
-		host,
-		port,
-		user,
-		password,
-		dbName,
-		sslMode,
-	)
-
-	db, err := sql.Open("postgres", dsn)
-	if err != nil {
-		return nil, err
-	}
-
-	if err := db.Ping(); err != nil {
-		return nil, err
-	}
-
-	fmt.Println("Connected to PostgreSQL")
-	return db, nil
 }
---
+++ internal/infrastructure/database/postgres.go
@@ -0,0 +1,46 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/lib/pq"
+)
+
+func ConnectPostgres() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("DB_PORT")
+	if port == "" {
+		port = "5432"
+	}
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbName := os.Getenv("DB_NAME")
+	sslMode := os.Getenv("DB_SSLMODE")
+
+	dsn := fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		host,
+		port,
+		user,
+		password,
+		dbName,
+		sslMode,
+	)
+
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+
+	fmt.Println("Connected to PostgreSQL")
+	return db, nil
+}
