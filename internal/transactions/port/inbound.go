1868d9e53c791cc2b4b54b4e8bfbee6fbb390d10
panipak <panipaksenmee@gmail.com>
2026-05-29 14:42:08 +0700

    enhance: [MPA-9] refactor Transaction port inbound, outbound

--- internal/transactions/port/inbound.go
+++ internal/transactions/port/inbound.go
@@ -3,5 +3,5 @@ package port
 import "mini-project-a/internal/transactions/core/entity"
 
 type TransactionsService interface {
-	CreateTransaction(transaction *entity.Transactions) error
+	CreateTransaction(transaction *entity.Transactions) (*entity.Transactions, error)
 }
