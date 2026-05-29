50106494c78d7d4f843f941c02e2ecb6452394e3
panipak <panipaksenmee@gmail.com>
2026-05-29 14:42:31 +0700

    enhance: [MPA-9] refactor Transaction adapter outbound

--- internal/transactions/adapter/outbound/postgres/transactions_respository.go
+++ internal/transactions/adapter/outbound/postgres/transactions_respository.go
@@ -1,34 +1,36 @@
 package postgres
 
 import (
-	"database/sql"
 	"mini-project-a/internal/transactions/core/entity"
+
+	"github.com/jmoiron/sqlx"
 )
 
 type TransactionsPostgresRepository struct {
-	db *sql.DB
+	db *sqlx.DB
 }
 
-func NewTransactionsPostgresRepository(db *sql.DB) *TransactionsPostgresRepository {
+func NewTransactionsPostgresRepository(db *sqlx.DB) *TransactionsPostgresRepository {
 	return &TransactionsPostgresRepository{db: db}
 }
 
-func (r *TransactionsPostgresRepository) CreateTransaction(transaction *entity.Transactions) error {
+func (r *TransactionsPostgresRepository) CreateTransaction(transaction *entity.Transactions) (*entity.Transactions, error) {
 	model := FromEntity(transaction)
-	query := `INSERT INTO transactions (account_id, transaction_type, amount, balance_before, balance_after, description, created_at)
-	VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING id`
-	err := r.db.QueryRow(query,
-		model.AccountID,
-		model.TransactionType,
-		model.Amount,
-		model.BalanceBefore,
-		model.BalanceAfter,
-		model.Description,
-		model.CreatedAt,
-	).Scan(&model.ID)
+	query := `INSERT INTO transactions (account_id, transaction_type, amount, balance_before, balance_after, description)
+	VALUES (:account_id, :transaction_type, :amount, :balance_before, :balance_after, :description)`
+	rows, err := r.db.NamedQuery(query, model)
 	if err != nil {
-		return err
+		return nil, err
 	}
-	transaction.ID = model.ID
-	return nil
+	defer rows.Close()
+
+	var output Transactions
+	if rows.Next() {
+		err = rows.StructScan(&output)
+		if err != nil {
+			return nil, err
+		}
+	}
+	result := output.ToEntity()
+	return result, nil
 }
