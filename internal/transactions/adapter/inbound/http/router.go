4db400131cf74d60421de3149f882c4c66e858de
panipak <panipaksenmee@gmail.com>
2026-06-09 15:59:24 +0700

    refactor: [MPA-12] Rename variable for clarity in TransactionsRoutes function

--- internal/transactions/adapter/inbound/http/router.go
+++ internal/transactions/adapter/inbound/http/router.go
@@ -3,6 +3,6 @@ package http
 import "github.com/gin-gonic/gin"
 
 func TransactionsRoutes(router *gin.RouterGroup, transactionsHandler *TransactionsHandler) {
-	accounts := router.Group("/accounts")
-	accounts.POST("/:accountNumber/deposit", transactionsHandler.CreateDeposit)
+	transactions := router.Group("/accounts")
+	transactions.POST("/:accountNumber/deposit", transactionsHandler.CreateDeposit)
 }
