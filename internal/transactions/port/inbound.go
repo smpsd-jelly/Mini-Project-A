818b3bed416eee7ddf5e34704ab6b2c3eca5f646
panipak <panipaksenmee@gmail.com>
2026-06-10 10:29:58 +0700

    feature: [MPA-13] Add Withdraw methods to TransactionsService interface

--- internal/transactions/port/inbound.go
+++ internal/transactions/port/inbound.go
@@ -7,4 +7,6 @@ import (
 type TransactionsService interface {
 	CreateTransaction(transaction *entity.Transactions) (*entity.Transactions, error)
 	CreateInitialDeposit(accountID int64, amount float64) error
+	Deposit(accountNumber string, transaction *entity.Transactions) (*entity.Transactions, error)
+	Withdraw(accountNumber string, transaction *entity.Transactions) (*entity.Transactions, error)
 }
